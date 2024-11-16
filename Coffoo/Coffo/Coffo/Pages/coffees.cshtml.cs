b4c77b4a2f25e46aea54206e62f5b5f2fea2385a
superrboss <osama1054231@gmail.com>
2024-11-16 20:24:34 +0200

    Coffo Project

---
+++ Coffoo/Coffo/Coffo/Pages/coffees.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coffo.Pages
+{
+    public class coffeesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
